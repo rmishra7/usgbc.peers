92bcb7c68065c780312683c7082de29bf775c450
Roshan Mishra <rmishra@usgbc.org>
2016-08-08 12:50:41 +0530

    mysql and requirement file updated

--- peers/peers/settings.py
+++ peers/peers/settings.py
@@ -95,10 +95,21 @@ WSGI_APPLICATION = 'peers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'peers',
+        'USER': 'root',
+        'PASSWORD': 'root@123',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
