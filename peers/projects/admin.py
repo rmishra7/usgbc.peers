cb0d1506eded76145db52a1c47569e3388dd0321
Roshan Mishra <rmishra@usgbc.org>
2016-08-17 01:57:57 +0530

    models meta verbose updated

--- peers/projects/admin.py
+++ peers/projects/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
 from .models import (
-    Project, CreditsAchieved, CreditsValueMapping, Strategy, StrategyQuestion, ProjectStrategy,
-    ElectricityPlant, ElectricityPlantUnit, ProjectPlant, ProjectPlantUnit, CreditsKeyword,
-    ProjectSpecificInfo)
+    Project, ProjectSpecificInfo, CreditsAchieved, CreditsValueMapping, Strategy,
+    StrategyQuestion, ProjectStrategy, ElectricityPlant, ElectricityPlantUnit,
+    ProjectPlant, ProjectPlantUnit, CreditsKeyword
+    )
 
 models = [
-    Project, CreditsAchieved, CreditsValueMapping, Strategy, StrategyQuestion, ProjectStrategy,
-    ElectricityPlant, ElectricityPlantUnit, ProjectPlant, ProjectPlantUnit, CreditsKeyword,
-    ProjectSpecificInfo]
+    Project, ProjectSpecificInfo, CreditsAchieved, CreditsValueMapping, Strategy,
+    StrategyQuestion, ProjectStrategy, ElectricityPlant, ElectricityPlantUnit,
+    ProjectPlant, ProjectPlantUnit, CreditsKeyword]
 
 admin.site.register(models)
